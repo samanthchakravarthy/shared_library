56da7dabc1ddb79fbf9b5b34d9a5b3f296171050
manaraya <nmaheswari%40gmail.com>
2019-11-27 22:32:13 +0530

    run the sh script

--- resources/com/library/scripts/main.sh
+++ resources/com/library/scripts/main.sh
@@ -5,12 +5,12 @@ set username = $1
 set user = $2
 set password = $3
 set reponame = $4
-set scm_repocreation = $5
+set repocreation = $5
 
 
 if [ "$repocreation" = true ] ; then
 set +x      # turn off echo
-sh resources/com/library/scripts/bitbucket.sh $username $user $password $reponame
+bash resources/com/library/scripts/bitbucket.sh $username $user $password $reponame
 set -x      # turn on echo
 echo "Creation of repo completed"
 sleep 10
