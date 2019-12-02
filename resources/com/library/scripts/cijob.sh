c36398b12360263a3fc98c6699c62e499d613d72
manaraya <nmaheswari%40gmail.com>
2019-12-02 15:45:24 +0530

    adding server details

--- resources/com/library/scripts/cijob.sh
+++ resources/com/library/scripts/cijob.sh
@@ -6,5 +6,5 @@ set jenkinsUrl = $2
 set credential_Id = $3
 
 
-CRUMB=$(curl -s 'http://example.com/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "admin":"11fc632b09259098c02a1f1bfc5b794040")
+CRUMB=$(curl -s 'http://54.164.191.28:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "admin":"11fc632b09259098c02a1f1bfc5b794040")
 echo $CRUMB
