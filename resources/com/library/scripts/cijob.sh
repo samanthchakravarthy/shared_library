2b93a0b8360735dc50f3646a99a209589824eca4
manaraya <nmaheswari%40gmail.com>
2019-12-02 15:54:43 +0530

    get the config file

--- resources/com/library/scripts/cijob.sh
+++ resources/com/library/scripts/cijob.sh
@@ -5,6 +5,14 @@ set reponame = $1
 set jenkinsUrl = $2
 set credential_Id = $3
 
+username="admin"
+API_TOKEN="11fc632b09259098c02a1f1bfc5b794040"
 
-CRUMB=$(curl -s 'http://54.164.191.28:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "admin":"11fc632b09259098c02a1f1bfc5b794040")
+
+CRUMB=$(curl -s '$jenkinsUrl/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u username:API_TOKEN)
 echo $CRUMB
+
+curl -X GET '$jenkinsUrl/job/java-pipeline/config.xml' -u username:API_TOKEN -o mylocalconfig.xml
+
+ls -ltR
+cat ylocalconfig.xml
