6ee8b00fb52e48cd1e76520da2d6ae0c346a5b50
manaraya <nmaheswari%40gmail.com>
2019-12-02 17:04:02 +0530

    adding quotes

--- resources/com/library/scripts/cijob.sh
+++ resources/com/library/scripts/cijob.sh
@@ -10,10 +10,10 @@ API_TOKEN="11fc632b09259098c02a1f1bfc5b794040"
 
 echo "hello"
 
-CRUMB=$(curl -s '$jenkinsUrl/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u $username:$API_TOKEN)
+CRUMB=$(curl -s '"'$jenkinsUrl'"/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "$username":"$API_TOKEN")
 echo $CRUMB
 
-curl -X GET '$jenkinsUrl/job/java-pipeline/config.xml' -u $username:$API_TOKEN -o mylocalconfig.xml
+curl -X GET '"'$jenkinsUrl'"/job/java-pipeline/config.xml' -u "$username":"$API_TOKEN" -o mylocalconfig.xml
 
 ls -ltR
 cat ylocalconfig.xml
