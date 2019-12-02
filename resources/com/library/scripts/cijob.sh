ad40390d6dde49b32656823ad01b160c1c2059f6
manaraya <nmaheswari%40gmail.com>
2019-12-02 19:10:53 +0530

    changed the xml file name

--- resources/com/library/scripts/cijob.sh
+++ resources/com/library/scripts/cijob.sh
@@ -11,9 +11,9 @@ API_TOKEN="11fc632b09259098c02a1f1bfc5b794040"
 echo "hello"
 
 
-CRUMB=$(curl -s -u "$username":"$API_TOKEN" "$jenkinsUrl/crumbIssuer/api/xml")
+#CRUMB=$(curl -s -u "$username":"$API_TOKEN" "$jenkinsUrl/crumbIssuer/api/json")
 
-#CRUMB=$(curl -s ''$jenkinsUrl'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "$username":"$API_TOKEN")
+CRUMB=$(curl -s ''$jenkinsUrl'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u "$username":"$API_TOKEN")
 
 
 echo $CRUMB
@@ -28,9 +28,9 @@ echo "reading a file"
 
 while read a; do
     echo ${a//"snowball.git"/"$reponame.git"}
-done < mylocalconfig.xml > mylocalconfig.xml.t
-mv mylocalconfig.xml{.t,}
+done < mylocalconfig.xml > localconfig.xml
+#mv mylocalconfig.xml{.t,}
 
 #cat mylocalconfig.xml
 
-curl -s -XPOST "$jenkinsUrl/createItem?name=yourJobName" -u "$username":"$API_TOKEN" --data-binary @mylocalconfig.xml -H "$CRUMB" -H "Content-Type:text/xml"
+curl -s -XPOST "$jenkinsUrl/createItem?name=yourJobName" -u "$username":"$API_TOKEN" --data-binary @localconfig.xml -H "$CRUMB" -H "Content-Type:text/xml"
