e652ebffa21b3269abc26ed5dc4e5b39e03fb7f2
Srikanth g <srikanth.gunuputi@gmail.com>
2020-10-01 13:57:59 +0000

    regressionTests.groovy edited online with Bitbucket

--- vars/regressionTests.groovy
+++ vars/regressionTests.groovy
@@ -1,3 +1,17 @@
 def executeRegressionTests() {
     print('Executing Regression tests')
+    node {
+        stage('Checkout Tests') {
+            print('Cloing the Regression tests from Bitbucket')
+        }
+        stage('Prepare Environment') {
+            print('Preparing Environment for tests')
+        }
+        stage('Execute Tests') {
+            print('Executing Regression tests')
+        }
+        stage('Notification') {
+            print('Notifying via team')
+        }
+    }
 }
