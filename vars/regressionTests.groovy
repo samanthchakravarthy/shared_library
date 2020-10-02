967ac619a6f43b111acf5583d5c407c5f0f827c1
Srikanth g <srikanth.gunuputi@gmail.com>
2020-10-02 04:16:50 +0000

    regressionTests.groovy edited online with Bitbucket

--- vars/regressionTests.groovy
+++ vars/regressionTests.groovy
@@ -22,9 +22,9 @@ def executeRegressionTests(heal_enabled, levels, browser) {
             try {
                 //sh "docker run -v `pwd`/reports:/opt/robotframework/reports:Z -v `pwd`/project:/opt/robotframework/tests:Z -e BROWSER=$browser -e AWS_ACCESS_KEY_ID=AKIA2EMYZUE52BZL7B6A -e AWS_SECRET_ACCESS_KEY=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 -e levels=$levels -e healEnabled=$healValue -e ROBOT_THREADS=2 healer"
                 sh 'aaaa'                
-            } finally {
-                print(currentBuild.result)
-            }
+            } catch (Excepion e) {
+                currentBuild.result = 'FAILURE'
+            }            
         }
         stage('Prepare Reports') {
             robot outputPath: '/var/lib/jenkins/workspace/test-pipeline/reports', logFileName: 'log.html', outputFileName: 'output.xml', reportFileName: 'report.hml', passThreshold: 100, unstableThreshold: 75.0
