894f90e9587639bf251fcb66aa70a7c09298508d
Srikanth g <srikanth.gunuputi@gmail.com>
2020-10-12 10:45:58 +0000

    regressionTests.groovy edited online with Bitbucket

--- vars/regressionTests.groovy
+++ vars/regressionTests.groovy
@@ -20,7 +20,7 @@ def executeRegressionTests(heal_enabled, levels, browser) {
         stage('Execute Tests') {
             print('Executing Regression tests')            
             try {
-                sh "docker run -v `pwd`/reports:/opt/robotframework/reports:Z -v `pwd`/project:/opt/robotframework/tests:Z -e BROWSER=$browser -e AWS_ACCESS_KEY_ID=AKIA2EMYZUE52BZL7B6A -e AWS_SECRET_ACCESS_KEY=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 -e levels=$levels -e healEnabled=$healValue -e ROBOT_THREADS=2 healer"                       
+                sh "docker run -v `pwd`/reports:/opt/robotframework/reports:Z -v `pwd`/project:/opt/robotframework/tests:Z -e BROWSER=$browser -e AWS_ACCESS_KEY_ID=AKIA2EMYZUE52BZL7B6A -e AWS_SECRET_ACCESS_KEY=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 -e levels=$levels -e healEnabled=$healValue healer"                       
             } catch (Exception e) {
                 print ("Inside Catch block")
                 currentBuild.result = 'FAILURE'
