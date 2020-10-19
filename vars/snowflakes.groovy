e64b06870aba8f2bacf827bc1aebe77650bd5a65
Srikanth g <srikanth.gunuputi@gmail.com>
2020-10-19 07:27:40 +0000

    snowflakes.groovy created online with Bitbucket

---
+++ vars/snowflakes.groovy
@@ -0,0 +1,34 @@
+node {
+   
+   stage('Checkout') { 
+      checkout scm:[
+        $class: 'GitSCM', 
+        branches: [[name: 'master']], 
+        doGenerateSubmoduleConfigurations: false, 
+        extensions: [[$class: 'CleanCheckout']], 
+        submoduleCfg: [], 
+        userRemoteConfigs: [[credentialsId: 'Bitbucket', url: 'https://haisrig@bitbucket.org/haisrig/sf_analytics.git']]
+    ]
+   }
+   stage('MergeConflict') {
+       echo "Executing Merge Conflict..."
+       sleep 1
+       echo "Merge Conflict check is completed"
+   }
+   stage('Static Code Analysis') {
+        def scannerHome = tool 'SonarQube Scanner'
+        withSonarQubeEnv('sonar') {
+            sh """
+                ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=sf_analysis_cg -Dsonar.organization=haisrig-github
+            """
+        }
+   }
+   stage('Deploy') {
+       sh 'ls -ltR'
+       withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'Snowflakes', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+        sh 'flyway -url=jdbc:snowflake://https://xg81978.southeast-asia.azure.snowflakecomputing.com/?db=DEMO_DB -user=$USERNAME -password=$PASSWORD -locations=filesystem:sql info'
+        }
+   }
+   
+   
+}
